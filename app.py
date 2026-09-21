c6403878a7e8786373ff6f12ef9be177d420cccd
ploynat14112550-glitch <ploynat14112550@gmail.com>
2026-09-21 13:40:12 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+
+st.set_page_config(page_title="EDS2 Project")
+
+st.title("EDS2 Project")
+st.write("This is a sample Streamlit application.")
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+
+st.subheader("My first DataFrame")
+st.dataframe(df, width="stretch", hide_index=True)
